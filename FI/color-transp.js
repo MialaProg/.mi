1e4314a004702e9e58ea379836efeb3ad231f4ec
Miala <miala@ik.me>
2024-05-12 20:14:21 +0200

    tr

--- FI/color-transp.js
+++ FI/color-transp.js
@@ -1,26 +1,22 @@
-var img = document.getElementById('img');
-img.onchange = e => {
-    var reader = new FileReader();
-    reader.onload = readerEvent => {
-        img = new Image();
-        img.src = readerEvent.target.result;
-    };
-    reader.readAsDataURL(e.target.files[0]);
-};
-img.click();
+var img = new Image();
+
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-
-img.onload = function() {
+document.getElementById('img').addEventListener('change', function () {
+	img.src = URL.createObjectURL(this.files[0]);
+});
+img.onload = function () {
+	canvas.width = img.width;
+	canvas.height = img.height;
 	ctx.drawImage(img, 0, 0);
 };
 
-var original = function() {
+var original = function () {
 	ctx.drawImage(img, 0, 0);
 };
 
-var sepia = function() {
+var sepia = function () {
 	ctx.drawImage(img, 0, 0);
 	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	const data = imageData.data;
@@ -34,45 +30,51 @@ var sepia = function() {
 	ctx.putImageData(imageData, 0, 0);
 }
 
-var invert = function() {
+var invert = function () {
 	ctx.drawImage(img, 0, 0);
 	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	const data = imageData.data;
 	for (var i = 0; i < data.length; i += 4) {
-		data[i]     = 255 - data[i];     // red
+		data[i] = 255 - data[i];     // red
 		data[i + 1] = 255 - data[i + 1]; // green
 		data[i + 2] = 255 - data[i + 2]; // blue
 	}
 	ctx.putImageData(imageData, 0, 0);
 };
 
-var grayscale = function() {
+var grayscale = function () {
 	ctx.drawImage(img, 0, 0);
 	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 	const data = imageData.data;
 	for (var i = 0; i < data.length; i += 4) {
 		var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-		data[i]     = avg; // red
+		data[i] = avg; // red
 		data[i + 1] = avg; // green
 		data[i + 2] = avg; // blue
 	}
 	ctx.putImageData(imageData, 0, 0);
 };
 
-var transp = function() {
+var transp = function () {
 	ctx.drawImage(img, 0, 0);
-	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	const data = imageData.data;
+	let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	let data = imageData.data;
+	let nb = document.getElementById('nb').value;
+
+	let txt = document.getElementsByTagName('p');
 	for (var i = 0; i < data.length; i += 4) {
-		var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-		data[i+4] = parseInt(avg < document.getElementById('nb').value);
+		let avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+		data[i + 3] =(avg < nb) ? 0 : 255;
+		if (i%80 == 0){
+			txt[0].innerHTML = i / data.length * 100 + "%";
+		}
 	}
 	ctx.putImageData(imageData, 0, 0);
 };
 
 const inputs = document.querySelectorAll('[name=color]');
 for (const input of inputs) {
-	input.addEventListener("change", function(evt) {
+	input.addEventListener("change", function (evt) {
 		switch (evt.target.value) {
 			case "inverted":
 				return invert();
