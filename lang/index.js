87e8b962c247f3c41d93812440375322d7ccf013
Miala <miala+gitBdg@ik.me>
2025-03-28 22:11:29 +0100

    Ajout de la prise en charge de MiFont et mise à jour de la page de chargement

---
+++ lang/index.js
@@ -0,0 +1,80 @@
+var dico = false;
+var indexJS = false;
+
+async function fetchCSV() {
+    try {
+        const response = await fetch('./dico.fimi');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const text = await response.text();
+        // console.log("CSV content:", text);
+        dico = text.split('\n').map(line => line.split(';'));
+    } catch (error) {
+        console.error("Error fetching the CSV file:", error);
+    }
+}
+
+fetchCSV();
+
+
+// Wait until all content is loaded
+document.addEventListener("DOMContentLoaded", () => {
+    const waitForConvertMiFont = () => {
+        if (dico && typeof convertMiFont === "function" && indexJS) {
+            code();
+        } else {
+            setTimeout(waitForConvertMiFont, 100);
+        }
+    };
+
+    waitForConvertMiFont();
+});
+
+function code() {
+    
+}
+
+function createTable(list, cols){
+    var table = document.createElement('table');
+    table.classList.add('table');
+    // var thead = document.createElement('thead');
+    var tbody = document.createElement('tbody');
+    // var tr = document.createElement('tr');
+    // var th = document.createElement('th');
+    // th.textContent = 'Code';
+    // tr.appendChild(th);
+    // th = document.createElement('th');
+    // th.textContent = 'Char';
+    // tr.appendChild(th);
+    // thead.appendChild(tr);
+    // table.appendChild(thead);
+    list.forEach((item) => {
+        tr = document.createElement('tr');
+        // French
+        var td = document.createElement('td');
+        td.textContent = item[0];
+        tr.appendChild(td);
+        // Mi
+        td = document.createElement('td');
+        abbr = document.createElement('abbr');
+        abbr.title = item[1];
+        abbr.textContent = item[1];
+        td.appendChild(abbr);
+        convertMiFont(td);
+        tr.appendChild(td);
+
+        tbody.appendChild(tr);
+    });
+    table.appendChild(tbody);
+    return table;
+}
+
+
+
+
+
+
+
+
+indexJS = true;
