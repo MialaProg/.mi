929d2ba17b314e95ef775656f6ddfa7016793940
Miala <miala+gitBdg@ik.me>
2025-03-29 18:34:34 +0100

    theme switching

--- lang/index.js
+++ lang/index.js
@@ -30,6 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     waitForConvertMiFont();
+
+    let savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        let html = document.querySelector('html');
+        html.classList.remove('theme-dark', 'theme-light');
+        html.classList.add(savedTheme === 'dark' ? 'theme-dark' : 'theme-light');
+    }
 });
 
 function code() {
@@ -75,9 +82,18 @@ function createTable(list){
 }
 
 
-
-
-
+// Function to switch the theme
+function changeTheme() {
+    var html = document.querySelector('html');
+    if (html.classList.contains('theme-dark')) {
+        html.classList.remove('theme-dark');
+        html.classList.add('theme-light');
+    } else {
+        html.classList.remove('theme-light');
+        html.classList.add('theme-dark');
+    }
+    localStorage.setItem('theme', html.classList.contains('theme-dark') ? 'dark' : 'light');
+}
 
 
 
